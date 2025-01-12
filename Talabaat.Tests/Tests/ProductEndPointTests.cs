c42de036b792c03889424c1672affc008e6b3917
hazemzain <hazemzain1999@gmail.com>
2025-01-12 10:12:23 +0200

    Implement some testCases and apply Fluent Assertions

--- Talabaat.Tests/Tests/ProductEndPointTests.cs
+++ Talabaat.Tests/Tests/ProductEndPointTests.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework.Legacy;
 using Talabaat.APIs.Controllers;
@@ -55,5 +56,81 @@ namespace Talabaat.Tests.Test
             Assert.That(returnedBrands.Count(),Is.EqualTo(mockBrands.Count));
             CollectionAssert.AreEquivalent(mockBrands, returnedBrands);
         }
+
+        [Test]
+        public async Task GetBrand_ShouldReturnEmptyList_WhenNoBrandsAvalibleAsync()
+        {
+           var MockBrand = new List<ProductBrand>();
+           _mockBrandRepo.Setup(x => x.GetAllAsync()).ReturnsAsync(MockBrand);
+           var result = await _controller.GetBrands();
+           Assert.That(result, Is.Empty);
+           var okResult = result.Result as OkObjectResult;
+           Assert.That(okResult.StatusCode, Is.EqualTo(200));
+
+        }
+        //it will fail becouse End Point not handle Throw Exception
+        // i will handle it 
+        [Test]
+        public async Task GetBrand_ShouldReturnInternalServerError_WhenRepositoryThrowsException()
+        {
+            // Arrange
+            _mockBrandRepo.Setup(repo => repo.GetAllAsync()).ThrowsAsync(new Exception("Repository error"));
+
+            // Act
+            var result = await _controller.GetBrands();
+
+            // Assert
+            var objectResult = result.Result as ObjectResult;
+            Assert.That(objectResult, Is.Not.Null);
+            Assert.That(objectResult.StatusCode, Is.EqualTo(500));
+            Assert.That(objectResult.Value, Is.EqualTo("An error occurred while processing your request."));
+        }
+
+        [Test]
+        public async Task GetBrand_ShouldHandleLargeNumberOfData()
+        {
+            var mockBrands = Enumerable.Range(1, 1000).Select(i => new ProductBrand
+            {
+                Id = i,
+                Name = $"Brand{i}"
+            }).ToList();
+            _mockBrandRepo.Setup(x => x.GetAllAsync()).ReturnsAsync(mockBrands);
+            var Result = _controller.GetBrands();
+            var okResult = (await Result).Result as OkObjectResult;
+            Assert.That(okResult, Is.Not.Null);
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+            Assert.That(okResult.Value, Is.InstanceOf<IEnumerable<ProductBrand>>());
+            var returnedBrands = okResult.Value as IEnumerable<ProductBrand>;
+            Assert.That(returnedBrands.Count(), Is.EqualTo(mockBrands.Count));
+            CollectionAssert.AreEquivalent(mockBrands, returnedBrands);
+
+        }
+
+        [Test]
+        public async Task GetBrand_ShouldHandleLargeNumberOfDataUsingFluentAssertions()
+        {
+            var mockBrands = Enumerable.Range(1, 1000).Select(i => new ProductBrand
+            {
+                Id = i,
+                Name = $"Brand{i}"
+            }).ToList();
+            _mockBrandRepo.Setup(x => x.GetAllAsync()).ReturnsAsync(mockBrands);
+            var Result = _controller.GetBrands();
+            var okResult = (await Result).Result as OkObjectResult;
+            okResult.Should().NotBe(null);
+           // Assert.That(okResult, Is.Not.Null);
+           okResult.StatusCode.Should().Be(200);
+           okResult.Value.Should().BeAssignableTo<IEnumerable<ProductBrand>>()
+               .Which.Should().HaveCount(mockBrands.Count)
+               .And.BeEquivalentTo(mockBrands);
+            // Assert.That(okResult.StatusCode, Is.EqualTo(200));
+            Assert.That(okResult.Value, Is.InstanceOf<IEnumerable<ProductBrand>>());
+            var returnedBrands = okResult.Value as IEnumerable<ProductBrand>;
+            returnedBrands.Count().Should().Be(mockBrands.Count);
+            //Assert.That(returnedBrands.Count(), Is.EqualTo(mockBrands.Count));
+           // CollectionAssert.AreEquivalent(mockBrands, returnedBrands);
+
+        }
+
     }
 }
